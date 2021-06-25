ca128781f9af62db640eafe2f7bc26f382bd6309
Kasimir <kzi-eckhardt@web.de>
2021-06-25 16:25:50 +0200

    fixed PersonController

--- src/main/java/webshop/Person.java
+++ src/main/java/webshop/Person.java
@@ -1,10 +1,12 @@
 package webshop;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor  // needed for getting form-inputs via POST and thymeleaf
 public class Person {
     private Long id;
     private String firstname;
--- src/main/java/webshop/PersonController.java
+++ src/main/java/webshop/PersonController.java
@@ -8,14 +8,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PersonController {
-
+    /**
+     * 
+     * @param model
+     * @return home.html template
+     */
     @GetMapping("/")
     public String home(Model model) {
         Person person = Person.builder().firstname("Alfred").lastname("Neumann").build();
         model.addAttribute(person);
         return "home";
     }
-
+    /**
+     * 
+     * @param model
+     * @return register.html template
+     */
     @GetMapping("/register")
     public String register(Model model){
         Person person = Person.builder().build();
@@ -23,9 +31,15 @@ public class PersonController {
         return "register";
     }
 
+    /**
+     * 
+     * @param person built automatically with submitted values
+     * @param model
+     * @return registered.html template
+     */
     @PostMapping("/register")
-    public String register(@ModelAttribute Person person, Model model){
-        model.addAttribute(person); // womöglich überflüssig an dieser Stelle
+    public String registered(@ModelAttribute Person person, Model model){
+        model.addAttribute(person);
         // hier sollte der user in der Datenbank gespeichert werden.
         return "registered";
     }
