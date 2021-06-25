811f0924fd8ff94a9b2d369c31745cc1d15dfb4f
Kasimir <kzi-eckhardt@web.de>
2021-06-25 14:52:52 +0200

    methode auskommentiert, um Fehler zu vermeiden

--- src/main/java/webshop/JDBCTemplateController.java
+++ src/main/java/webshop/JDBCTemplateController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class JDBCTemplateController {
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-    @GetMapping("/{id}")
-    @ResponseBody
-    public String home(@PathVariable Long id) {
-        Person person = jdbcTemplate.queryForObject("SELECT * FROM Person WHERE Id = ?", new PersonRowMapper(), new Object[] {id});
-        return person.toString();
-    }
+    // @Autowired
+    // JdbcTemplate jdbcTemplate;
+    // @GetMapping("/{id}")
+    // @ResponseBody
+    // public String home(@PathVariable Long id) {
+    //     Person person = jdbcTemplate.queryForObject("SELECT * FROM Person WHERE Id = ?", new PersonRowMapper(), new Object[] {id});
+    //     return person.toString();
+    // }
 }
