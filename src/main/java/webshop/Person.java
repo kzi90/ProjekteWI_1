4bc261877d753cc275c529fc84d0947f704353b5
Kasimir <kzi-eckhardt@web.de>
2021-06-27 01:39:01 +0200

    cosmetic corrections

--- src/main/java/webshop/Person.java
+++ src/main/java/webshop/Person.java
@@ -1,4 +1,5 @@
 package webshop;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/webshop/PersonController.java
+++ src/main/java/webshop/PersonController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class PersonController {
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
 
     /**
      * 
