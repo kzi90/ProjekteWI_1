65b3b59ea54b8adfc9d04624f0d0fcbaaf81bdb4
Kasimir <kzi-eckhardt@web.de>
2021-06-30 16:24:16 +0200

    tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,51 @@
 -- Das wird nicht ausgeführt, da eine persistente DB verwendet wird (database.mv.db)
 -- Trotzdem kann hier dokumentiert werden, welche Tabellen wie erstellt wurden.
 CREATE TABLE person (
-   id INTEGER NOT NULL AUTO_INCREMENT,
-   firstname VARCHAR(128) NOT NULL,
-   lastname VARCHAR(128) NOT NULL,
-   PRIMARY KEY (id)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(128) NOT NULL,
+    lastname VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE addresses (
-   
-)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    street VARCHAR(128) NOT NULL,
+    housenr VARCHAR(128) NOT NULL,
+    postcode INTEGER NOT NULL,
+    city VARCHAR(128) NOT NULL
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE customers (
-   id INTEGER NOT NULL AUTO_INCREMENT,
-   firstname VARCHAR(128) NOT NULL,
-   lastname VARCHAR(128) NOT NULL,
-   address_id INTEGER NOT NULL,
-   email VARCHAR(128) NOT NULL,
-   passhash VARCHAR(128) NOT NULL,
-   FOREIGN KEY (address_id) REFERENCES ADDRESSES(id),
-   PRIMARY KEY (id),
-   UNIQUE (email)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(128) NOT NULL,
+    lastname VARCHAR(128) NOT NULL,
+    address_id INTEGER NOT NULL,
+    email VARCHAR(128) NOT NULL,
+    pass_hash VARCHAR(128) NOT NULL,
+    FOREIGN KEY (address_id) REFERENCES ADDRESSES(id),
+    PRIMARY KEY (id),
+    UNIQUE (email)
+);
+
+CREATE TABLE orders (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    order_date DATE NOT NULL,
+    order_time TIME NOT NULL,
+    cust_id INTEGER NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (cust_id) REFERENCES customers(id)
+);
+
+CREATE TABLE products (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(128) NOT NULL,
+    product_description VARCHAR(512),
+    amount_ml INTEGER NOT NULL,
+    price_eur DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE orderpositions (
+        
 );
