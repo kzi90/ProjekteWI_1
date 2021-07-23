c404fddbfe7c6221ebdd0532cbb5509f4244e068
Kasimir <kzi-eckhardt@web.de>
2021-07-23 19:19:53 +0200

    improved ordercompletion

--- src/main/java/webshop/OrderController.java
+++ src/main/java/webshop/OrderController.java
@@ -1,7 +1,6 @@
 package webshop;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OrderController {
-    
+
     @Autowired
     private JdbcTemplate db;
 
@@ -23,66 +22,76 @@ public class OrderController {
 
     @GetMapping("/ordercompletion")
     public String ordercompletion(@CookieValue(value = "loggedInUser", defaultValue = "") String loggedInUser,
-                                  @CookieValue(value = "SessionID", defaultValue = "") String sessID,
-                                                                                        Model model){
-        if (loggedInUser.isEmpty()){
+            @CookieValue(value = "SessionID", defaultValue = "") String sessID, Model model) {
+        if (loggedInUser.isEmpty()) {
             return "redirect:/login";
         }
         model.addAttribute("loggedInUser", loggedInUser);
+
+        // get shoppingCart
         ShoppingCart shoppingCart = ShoppingCart.findBySessID(Integer.valueOf(sessID));
-        Customer customer = db.queryForObject("SELECT * FROM customers WHERE email = ?;", new CustomerRowMapper(), loggedInUser);
-        // build order
-        Order order = new Order();
-        order.setCustID(customer.getId());
-        order.setStatus("ordered");
 
-        // build orderpositions
-        List<OrderPosition> orderPositions = new ArrayList<>();
-        Product product;
-        Double lineTotal;
-        Double total = 0.0;
-        Integer posNr = 1;
-        for (ShoppingCartPosition pos : shoppingCart.getCartList()) {
-            product = db.queryForObject("SELECT * FROM products WHERE id = ?", new ProductRowMapper(), pos.getProductID());
-            lineTotal = Math.round(pos.getQuantity() * product.getPrice() * 100) / 100.0;
-            total += lineTotal;
-            orderPositions.add(new OrderPosition(posNr++, pos.getQuantity(), pos.getProductID(), 0));
+        if (!shoppingCart.getCartList().isEmpty()) {
+
+            // get customer via cookie
+            Customer customer = db.queryForObject("SELECT * FROM customers WHERE email = ?;", new CustomerRowMapper(),
+                    loggedInUser);
+
+            // build order
+            Order order = new Order();
+            order.setCustID(customer.getId());
+            order.setStatus("ordered");
+
+            // build orderpositions
+            List<OrderPosition> orderPositions = new ArrayList<>();
+            Product product;
+            Double lineTotal;
+            Double total = 0.0;
+            Integer posNr = 1;
+            for (ShoppingCartPosition pos : shoppingCart.getCartList()) {
+                product = db.queryForObject("SELECT * FROM products WHERE id = ?", new ProductRowMapper(),
+                        pos.getProductID());
+                lineTotal = Math.round(pos.getQuantity() * product.getPrice() * 100) / 100.0;
+                total += lineTotal;
+                orderPositions.add(new OrderPosition(posNr++, pos.getQuantity(), pos.getProductID(), 0));
+            }
+            total = Math.round(total * 100) / 100.0;
+            model.addAttribute("total", String.format("%.2f", total));
+            order.setTotalPrice(total);
+
+            // write order to database
+            order = saveOrder(order);
+            model.addAttribute("orderNumber", order.getId());
+
+            // write orderpositions to database
+            for (OrderPosition orderPosition : orderPositions) {
+                orderPosition.setOrderID(order.getId());
+                orderPositionController.saveOrderPos(orderPosition);
+            }
+
+            // clear shoppingCart
+            shoppingCart.setCartList(new ArrayList<>());
+
+            model.addAttribute("templateName", "ordercompletion");
+            model.addAttribute("title", "Bestellabschluss");
+            return "layout";
+        } else {
+            return "redirect:/";
         }
-        total = Math.round(total * 100) / 100.0;
-        model.addAttribute("total", String.format("%.2f", total));
-        order.setTotalPrice(total);
-
-        // write order to database
-        order = saveOrder(order);
-        model.addAttribute("orderNumber", order.getId());
-
-        // write orderpositions to database
-        for (OrderPosition orderPosition : orderPositions){
-            orderPosition.setOrderID(order.getId());
-            orderPositionController.saveOrderPos(orderPosition);
-        }
-
-        // clear shoppingCart
-        shoppingCart.setCartList(new ArrayList<>());
-
-        model.addAttribute("templateName", "ordercompletion");
-        model.addAttribute("title", "Bestellabschluss");
-        return "layout";
     }
 
-    public Order saveOrder(Order order){
+    public Order saveOrder(Order order) {
         Date dateTime = new Date();
         order.setOrderDate(new SimpleDateFormat("yyyy-MM-dd").format(dateTime));
         order.setOrderTime(new SimpleDateFormat("HH:mm:ss").format(dateTime));
         String saveOrderSQL = "INSERT INTO orders (order_date, order_time, total_price, "
-                                + "order_status, cust_id) VALUES (?, ?, ?, ?, ?);";
-        this.db.update(saveOrderSQL, order.getOrderDate(),
-                                     order.getOrderTime(),
-                                     order.getTotalPrice(),
-                                     order.getStatus(),
-                                     order.getCustID());
-        String getSQL = "SELECT * FROM orders WHERE order_date = ? AND order_time = ?;"; // Ändern, anhand MAX(ID) abfragen
-        order = db.queryForObject(getSQL, new OrderRowMapper(), order.getOrderDate(), order.getOrderTime());
+                + "order_status, cust_id) VALUES (?, ?, ?, ?, ?);";
+        this.db.update(saveOrderSQL, order.getOrderDate(), order.getOrderTime(), order.getTotalPrice(),
+                order.getStatus(), order.getCustID());
+        // the highest id is the one of the order, added one line before, because of
+        // the auto_increment function of the database
+        String getSQL = "SELECT * FROM orders WHERE id = (SELECT MAX(id) FROM orders);";
+        order = db.queryForObject(getSQL, new OrderRowMapper());
         return order;
     }
 
